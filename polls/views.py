032bb30efff4552e0cc6ff75b486146bb783181a
Konstantin Shchegolev <konstantinshchegolev@Konstantins-MacBook-Air.local>
2021-02-04 19:18:05 +0200

    registration

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,19 @@
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView
 
 # Create your views here.
+from django.views.generic import CreateView
 
 
 def success(request):
     return HttpResponse("SUCCESS!!!")
 
 
-class RegistrationForm(LoginView):
+class RegistrationForm(CreateView):
     model = User
-    template_name = "polls/login.html"
+    template_name = "polls/registration.html"
+    fields = ['username', 'email', 'password']
     success_url = '/polls/success/'
 
     def form_valid(self, form):
@@ -25,4 +26,4 @@ class RegistrationForm(LoginView):
             email=fake_email,
             password=passw
         )
-        return HttpResponseRedirect(self.get_success_url())
+        return redirect(self.success_url)
