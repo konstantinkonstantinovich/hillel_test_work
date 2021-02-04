15f4cb3da2c152e391daa05223848b7abce34900
kvantonio <kvantonio@i.ua>
2021-02-04 19:51:33 +0200

    logout

--- polls/urls.py
+++ polls/urls.py
@@ -9,5 +9,6 @@ app_name = 'polls'
 urlpatterns = [
     path('success/', views.success, name='success'),
     path('login/', views.LoginForm.as_view(), name='login'),
+    path('logout/', views.LogoutForm.as_view(), name='logout'),
     path('registrate/', views.LoginForm.as_view(), name='registrate'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
@@ -20,4 +19,17 @@ class LoginForm(LoginView):
         """Security check complete. Log the user in."""
         login(self.request, form.get_user())
         return HttpResponseRedirect(self.get_success_url())
+
+
+class LogoutForm(LogoutView):
+    model = User
+    template_name = "polls/logout.html"
+    success_url = '/polls/success/'
+
+    def form_valid(self, form):
+        """Security check complete. Log the user in."""
+
+        logout(self.request)
+
+        return HttpResponseRedirect(self.success_url)
 
